9a92a1921d2f5919a6b8468fcdf6ce821b11c5bd
srimul <60817863+srimul@users.noreply.github.com>
2020-02-17 16:00:20 -0600

    Included comments

--- shellscripts/bashScriptExcecises/script6.sh
+++ shellscripts/bashScriptExcecises/script6.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
- 
+# checks the size of the input file 
 if test -s $1
 then
         echo "$1 not empty file"
 fi
+# checks the input name is file or not
 if test -f $1
 then
         echo "$1 normal file. Not a directory"
 fi
+# checks the given file is available or not
 if test -e $1
 then 
     echo "$1 exists"
 fi 
+# checks the input name is directory or not
 if test -d $1
 then
         echo "$1 is directory and not a file"
 fi
+# checks the read permissions on input file
 if test -r $1
 then
         echo "$1 is read-only file"
 fi
+# checks the excutable permissions on input file
 if test -x $1
 then
         echo "$1 is executable"
