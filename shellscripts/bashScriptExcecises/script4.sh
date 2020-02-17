b82d335b078cadaec70bbd2bca6739d51cd0f5df
srimul <60817863+srimul@users.noreply.github.com>
2020-02-17 16:14:03 -0600

    Included comments

--- shellscripts/bashScriptExcecises/script4.sh
+++ shellscripts/bashScriptExcecises/script4.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#checks input value is less than 0
 if test $1 -lt 0
 then
         echo "$1 < 0"
 fi
- 
+#checks input value is less than or equal to 0
 if test $1 -le 0
 then
         echo "$1 <= 0"
