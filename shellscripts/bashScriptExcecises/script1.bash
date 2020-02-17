b90183085c5c2effa5c75cf178c5e1671ceb6fb1
srimul <60817863+srimul@users.noreply.github.com>
2020-02-17 09:08:46 -0600

    Included Comments

--- shellscripts/bashScriptExcecises/script1.bash
+++ shellscripts/bashScriptExcecises/script1.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "ls > file_list"
+#creates the file "file_list" with ls (list of files) output
 ls > file_list
  
 echo "ls -la >> file_list"
+#appends the file "file_list" with ls -la (includes hidden files) output
 ls -la >> file_list
  
 echo "cat < file_list"
+# prints the contents in file "file_list"
 cat < file_list
