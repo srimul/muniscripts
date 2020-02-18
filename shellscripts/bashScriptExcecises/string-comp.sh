2a12069ba19492400b0960376139e9c5aa147fe2
srimul <60817863+srimul@users.noreply.github.com>
2020-02-17 21:07:09 -0600

    Update and rename script5.sh to string-comp.sh

--- shellscripts/bashScriptExcecises/string-comp.sh
+++ shellscripts/bashScriptExcecises/string-comp.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 string_null=""
 string1="string1"
- 
-if [ $string_null -n ]
+#checks the string is null or not 
+if [ "$string_null" -n ]
 then
         echo "not null string"
 else
         echo "null string"
 fi
- 
-if [ $string_null -z ]
+#checks the length of the string length is zero true else flase 
+if [ "$string_null" -z ]
 then
         echo "null string"
 else
         echo "not null string"
 fi
- 
+ # checks the strings are equal or not
 if [ "$string_null" == "$string1" ]
 then
         echo "strings equal"
 else
         echo "strings not equal"
 fi
- 
+ # checks the strings are not equal
 if [ "$string_null" != "$string1" ]
 then
         echo "strings not equal"
