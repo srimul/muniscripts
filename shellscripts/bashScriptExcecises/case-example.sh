d06a94eed04131274eabd5a7145a90d0f393f636
srimul <60817863+srimul@users.noreply.github.com>
2020-02-20 09:51:30 -0600

    Update and rename script9.sh to case-example.sh

--- shellscripts/bashScriptExcecises/case-example.sh
+++ shellscripts/bashScriptExcecises/case-example.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#checks input value lenth and if lenth is zero sets the rental to *** Unknown vehicle ***
 if [ -z $1 ]
 then
   rental="*** Unknown vehicle ***"
-elif [ -n $1 ]
+elif [ -n $1 ] # checks the input value not null true then sets rental to input value
 then
   rental=$1
 fi
- 
+# checks the rental value and prints accordingly 
 case $rental in
    "car") echo "CAR";;
    "van") echo "VAN";;
